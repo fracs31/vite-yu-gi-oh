88e7292657e5b65b36152ec89fdc6f885b3518a4
fracs31 <pist.fracs95@gmail.com>
2023-03-03 14:13:51 +0100

    ADD header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,9 +4,27 @@
 
 <!-- Template -->
 <template>
-
+    <!-- Header -->
+    <header class="main-header">
+        <!-- Logo -->
+        <img class="header__logo" src="../assets/img/logo.jpg" alt="Logo">
+        <!-- Titolo -->
+        <h1 class="header__title">Yu-Gi-Oh Api</h1>
+    </header>
 </template>
 
 <!-- CSS -->
 <style scoped>
+    /* Header */
+    .main-header {
+        background-color: white;
+        padding: 20px;
+        display: flex;
+        align-items: center;
+        gap: 15px;
+    }
+    /* Logo */
+    .header__logo {
+        width: 100px;
+    }
 </style>
